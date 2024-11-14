f06e842d404293dd089da456db83e6ea2cb08d2e
Harshit7112 <harshmaurya7112@gmail.com>
2024-11-14 11:44:54 +0530

    msg

--- jswork/index.js
+++ jswork/index.js
@@ -107,13 +107,83 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 ///dom//////////////////
 // console.log("hi");
-let parent=document.getElementsByClassName("parent");
- console.log(document);
-console.log(parent);
-parent[0].innerText="data has changed";
-parent[0].innerHTML="<h2 style=color:red>data has changed</h2>";
+// let parent=document.getElementsByClassName("parent");
+// // console.log(document);
+// console.log(parent);
+// parent[0].innerText="data has changed";
+// parent[0].innerHTML="<h2 style=color:red>data has changed</h2>";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// const h1=document.createElement("h1");
+// console.log(h1);
+// h1.innerText="abes ec";
+// console.log(h1);
+// parent[0].appendChild(h1);
+// h1.style.backgroundColor="cyan";
+// h1.style.color="red";
+// h1.style.padding="20px";
+// h1.style.marginLeft="300px";
+// const img=document.createElement("img")
+// img.src="./wp6992126.jpg";
+// parent[0].appendChild(img);
+// img.setAttribute("height","300");
+// img.setAttribute("width","300");
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function getdata()
+// {
+//     console.log("hi,inside get data function");
+//      parent[0].innerHTML="<h2 style=color:red>hello bruther</h2>";
+// }
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// function getdata()
+// {
+//     console.log("hi,inside get data function");
+//      parent[0].innerHTML="<h2 style=color:red>hello bruther</h2>";
+// }
+// const button =document.getElementsByClassName("btn");
+// console.log(button);
+// button[0].addEventListener("click",getdata);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const promise =new Promise((resolve,reject)=>{
+//     let a=1;
+//     if(a>10)
+//     {
+//         resolve("Accepted");
+//     }
+//     else
+//     {
+//         reject("Not accepted");
+//     }
+// })
+// promise.then((msg)=>{console.log(msg)})
+// .catch((error)=>{console.log(error)})
+// .finally(()=>console.log("all resources hace closed"));
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// const promise2=new Promise((resolve,reject)=>{
+//     resolve({name:"harhsot",Stack:"Mern stack"})
+// });
+// promise2.then((data)=>{console.log("hi"+data.name)})
+// .catch(x=>{console.log(x)})
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const response=fetch("https://dummyjson.com/products")
+
+response.then((data)=>{
+    console.log(data);
+    data.json().then((res)=>
+    {
+        console.log(data);
+    })
+    }
+)
